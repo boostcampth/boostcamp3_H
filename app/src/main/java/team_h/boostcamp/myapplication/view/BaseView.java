1b88e7686d77cc9b8a448d328ed187bfb2031cc5
카퀴카퀴 <kokoxg2@naver.com>
2019-01-25 15:23:54 +0900

    - ê¸°ë³¸ íµ êµ¬ì¡ì¶ê° - Dependacny Add 	- Ted Permission 	- Glide 	- Room 	- CardView 	- RecyclerView - Databinding setting 	- mainActivity revise 	- gradle file revise

---
+++ app/src/main/java/team_h/boostcamp/myapplication/view/BaseView.java
@@ -0,0 +1,10 @@
+package team_h.boostcamp.myapplication.view;
+
+/*
+ * Created by Jongseong */
+public interface BaseView {
+
+    /*
+     * 토스트 메시지 출력 */
+    void showToastMessage(String message, int exposedTime);
+}
---
+++ app/src/main/java/team_h/boostcamp/myapplication/view/main/MainContractor.java
@@ -0,0 +1,15 @@
+package team_h.boostcamp.myapplication.view.main;
+
+import team_h.boostcamp.myapplication.view.BasePresenter;
+import team_h.boostcamp.myapplication.view.BaseView;
+
+public interface MainContractor {
+
+    interface View extends BaseView {
+
+    }
+
+    interface Presenter extends BasePresenter {
+
+    }
+}
