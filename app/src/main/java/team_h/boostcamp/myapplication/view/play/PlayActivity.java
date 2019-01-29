c8a203c4a1032883f38ad4e3f02d65da1d071308
Theodore <bethejustice@naver.com>
2019-01-29 23:14:57 +0900

    /#021_feat_playActivityxml

---
+++ app/src/main/java/team_h/boostcamp/myapplication/view/play/PlayActivity.java
@@ -0,0 +1,15 @@
+package team_h.boostcamp.myapplication.view.play;
+
+import androidx.appcompat.app.AppCompatActivity;
+import team_h.boostcamp.myapplication.R;
+
+import android.os.Bundle;
+
+public class PlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+    }
+}
