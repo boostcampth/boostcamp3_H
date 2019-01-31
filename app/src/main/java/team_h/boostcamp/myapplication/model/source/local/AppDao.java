e6d83a547dd4f40bf075d1036d5293d76a8fde22
카퀴카퀴 <kokoxg2@naver.com>
2019-01-31 20:09:22 +0900

    - 070_feat_save_record
    - 녹음된 아이템을 저장는 기능 수정

--- app/src/main/java/team_h/boostcamp/myapplication/model/source/local/AppDao.java
+++ app/src/main/java/team_h/boostcamp/myapplication/model/source/local/AppDao.java
@@ -14,7 +14,7 @@ import team_h.boostcamp.myapplication.model.Diary;
 public interface AppDao {
 
     @Query("SELECT * FROM diary LIMIT 10")
-    Single<List<Diary>> loadMoreDiary();
+    List<Diary> loadMoreDiary();
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insertDiary(Diary ...diaries);
