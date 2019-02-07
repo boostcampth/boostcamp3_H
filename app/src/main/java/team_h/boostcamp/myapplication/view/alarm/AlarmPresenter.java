4c44f52651bb56fb844abdfa1ec91be1ae530d71
Woo <jhsw0375@gmail.com>
2019-02-07 14:48:01 +0900

    #026_feat_알람 반복 설정 - 알람 반복 설정 구현

--- app/src/main/java/team_h/boostcamp/myapplication/view/alarm/AlarmPresenter.java
+++ app/src/main/java/team_h/boostcamp/myapplication/view/alarm/AlarmPresenter.java
@@ -34,6 +34,16 @@ public class AlarmPresenter implements AlarmContractor.Presenter {
 
     @Override
     public void setAlarm(Calendar calendar) {
+
+        final long currentTime = System.currentTimeMillis();
+        final long INTERVAL_TIME = 1000 * 60 * 60 * 24;
+        long userAlarmTime = calendar.getTimeInMillis();
+
+        // 설정한 시간이 현재 시간보다 작다면 다음 날 울리도록 INTERVAL_TIME 을 더한다.
+        if (currentTime > userAlarmTime) {
+            userAlarmTime += INTERVAL_TIME;
+        }
+
         // 알람 매니저가 AlertReceiver 라는 브로드 캐스트 리시버로 펜딩 인텐트로 날린다.
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(context, AlertReceiver.class);
@@ -41,16 +51,18 @@ public class AlarmPresenter implements AlarmContractor.Presenter {
 
         // 버전별 알람 매니저에게 시간을 설정.
         if (Build.VERSION.SDK_INT >= 23) {
-            alarmManager.setAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+            alarmManager.setAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, userAlarmTime, pendingIntent);
         } else {
             if (Build.VERSION.SDK_INT >= 19) {
-                alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+                alarmManager.setExact(AlarmManager.RTC_WAKEUP, userAlarmTime, pendingIntent);
             } else {
-                alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+                alarmManager.set(AlarmManager.RTC_WAKEUP, userAlarmTime, pendingIntent);
             }
         }
-        alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
-        // SharedPreference 저장하는 로직.
+        //alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, userAlarmTime, INTERVAL_TIME, pendingIntent);
+
+        // SharedPreference 저장하는 로직. String으로 저장.
         String timeText = DateFormat.getTimeInstance(DateFormat.SHORT).format(calendar.getTime());
         SharedPreference.getInstance().setPreferencePushTime(timeText);
     }
