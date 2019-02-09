8c0614f1053609df6b16ef84a02ece09b71beea9
Woo <jhsw0375@gmail.com>
2019-02-10 01:34:08 +0900

    issue-117-StaticeRepository로 통합

--- app/src/main/java/teamh/boostcamp/myapplication/data/repository/StaticsRepository.java
+++ app/src/main/java/teamh/boostcamp/myapplication/data/repository/StaticsRepository.java
@@ -4,10 +4,15 @@ import java.util.List;
 
 import androidx.annotation.NonNull;
 import io.reactivex.Single;
+import teamh.boostcamp.myapplication.data.model.CountedTag;
 import teamh.boostcamp.myapplication.data.model.EmotionHistory;
 
-public interface EmotionHistoryRepository {
+public interface StaticsRepository {
 
     @NonNull
     Single<List<EmotionHistory>> loadEmotionHistoryList();
+
+    @NonNull
+    Single<List<CountedTag>> loadCountedTagList();
+
 }
