9815a1e4bc6025bfea21a325caff0738e450b347
Theodore <bethejustice@naver.com>
2019-02-09 22:03:06 +0900

    recall class 수정

--- app/src/main/java/teamh/boostcamp/myapplication/data/model/Recall.java
+++ app/src/main/java/teamh/boostcamp/myapplication/data/model/Recall.java
@@ -1,24 +1,45 @@
 package teamh.boostcamp.myapplication.data.model;
 
+import java.util.List;
+
 import androidx.annotation.NonNull;
 
 public class Recall {
 
-    private String createdDate;
     @NonNull
+    private String startDate;
+    @NonNull
+    private String endDate;
     private int randomEmotion;
+    @NonNull
+    private List<Diary> diaryList;
 
-    public Recall(String createdDate, @NonNull int randomEmotion) {
-        this.createdDate = createdDate;
+    public Recall(@NonNull String startDate,
+                  @NonNull String endDate,
+                  int randomEmotion,
+                  @NonNull List<Diary> diaryList) {
+        this.startDate = startDate;
+        this.endDate = endDate;
         this.randomEmotion = randomEmotion;
-    }
-
-    public String getCreatedDate() {
-        return createdDate;
+        this.diaryList = diaryList;
     }
 
     @NonNull
+    public String getStartDate() {
+        return startDate;
+    }
+
+    @NonNull
+    public String getEndDate() {
+        return endDate;
+    }
+
     public int getRandomEmotion() {
         return randomEmotion;
+    }
+
+    @NonNull
+    public List<Diary> getDiaryList() {
+        return diaryList;
     }
 }
