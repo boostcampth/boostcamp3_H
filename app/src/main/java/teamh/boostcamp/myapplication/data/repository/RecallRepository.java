18352e9c017a1ca17f0100f2af20fc44870c54a4
Theodore <bethejustice@naver.com>
2019-02-09 23:37:02 +0900

    recallRepository review
    RecallRepository 수정
    
    mend

--- app/src/main/java/teamh/boostcamp/myapplication/data/repository/RecallRepository.java
+++ app/src/main/java/teamh/boostcamp/myapplication/data/repository/RecallRepository.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import androidx.annotation.NonNull;
 import io.reactivex.Single;
-import teamh.boostcamp.myapplication.data.model.Diary;
-import teamh.boostcamp.myapplication.data.model.Emotion;
 import teamh.boostcamp.myapplication.data.model.Recall;
 
 public interface RecallRepository {
@@ -13,6 +11,4 @@ public interface RecallRepository {
     @NonNull
     Single<List<Recall>> loadRecalls();
 
-    @NonNull
-    Single<List<Diary>> selectDiaries(@NonNull Emotion emotion);
 }
