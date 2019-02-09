80c2e5ecb9c03ae0c96588c7cb23b7837472a6ae
카퀴카퀴 <kokoxg2@naver.com>
2019-02-09 23:29:37 +0900

    - feature/issue-111-diary-repository-interface
    - interface 명세 작성
    - 요구사항에 맞게 변경

--- app/src/main/java/teamh/boostcamp/myapplication/data/repository/DiaryRepository.java
+++ app/src/main/java/teamh/boostcamp/myapplication/data/repository/DiaryRepository.java
@@ -3,18 +3,11 @@ package teamh.boostcamp.myapplication.data.repository;
 import java.util.List;
 
 import androidx.annotation.NonNull;
-import io.reactivex.Completable;
 import io.reactivex.Single;
 import teamh.boostcamp.myapplication.data.model.Diary;
 
 public interface DiaryRepository {
 
     @NonNull
-    Single<List<Diary>> loadMoreDiaries(final int idx);
-
-    @NonNull
-    Completable insertDiaries(@NonNull Diary ...diaries);
-
-    @NonNull
-    Completable truncateDiaries();
+    Single<List<Diary>> loadDiaryList(final int lastItemIndex, final int amount);
 }
