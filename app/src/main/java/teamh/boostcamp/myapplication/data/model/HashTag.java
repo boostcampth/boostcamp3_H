d563871193245c1e8b5c6250e29f052e964909e1
Woo <jhsw0375@gmail.com>
2019-02-09 20:55:20 +0900

    feature/issue-107-create-statics-data-class-HashTag 해시태그 데이터 클래스 작성

---
+++ app/src/main/java/teamh/boostcamp/myapplication/data/model/HashTag.java
@@ -0,0 +1,14 @@
+package teamh.boostcamp.myapplication.data.model;
+
+public class HashTag {
+
+    private String hashTagWord;
+
+    HashTag(String hashTagWord) {
+        this.hashTagWord = hashTagWord;
+    }
+
+    public String getHastagWord() {
+        return this.hashTagWord;
+    }
+}
