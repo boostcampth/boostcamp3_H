45a09cae745fe8fbc9ee2eca0c03d26c9d0c7471
Woo <jhsw0375@gmail.com>
2019-02-09 22:09:31 +0900

    issue-107-create-statics-data-class - 그래프 데이터 클래스 수정

--- app/src/main/java/teamh/boostcamp/myapplication/data/model/Statics.java
+++ app/src/main/java/teamh/boostcamp/myapplication/data/model/Statics.java
@@ -2,29 +2,28 @@ package teamh.boostcamp.myapplication.data.model;
 
 import com.github.mikephil.charting.data.Entry;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Statics {
 
-    private static final String SELECTED_EMOTIONS = "선택한 감정";
-    private static final String ANALYZED_EMOTIONS = "분석된 감정";
-
-    private List<Entry> thisWeekEmotions;
-    private List<Entry> lastWeekEmotions;
-
+    private int week; // 요일
+    private float emotion; // 감정
+    private List<Entry> weekEmotionData; // 요일과 감정으로 만든 하나의 좌표 데이터
 
     // 생성자를 통한 주입
-    public Statics(List<Entry> thisWeekEmotions, List<Entry> lastWeekEmotions) {
-        this.thisWeekEmotions = thisWeekEmotions;
-        this.lastWeekEmotions = lastWeekEmotions;
+    public Statics(int week, float emotion) {
+        weekEmotionData = new ArrayList<>();
+        this.week = week;
+        this.emotion = emotion;
+    }
+
+    public void createCoordinates(){
+        weekEmotionData.add(new Entry(week, emotion));
     }
 
     public List<Entry> getThisWeekEmotions() {
-        return this.thisWeekEmotions;
-    }
-
-    public List<Entry> getLastWeekEmotions() {
-        return this.lastWeekEmotions;
+        return this.weekEmotionData;
     }
 
 }
