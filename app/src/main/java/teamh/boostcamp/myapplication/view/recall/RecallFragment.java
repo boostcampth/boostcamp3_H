f987baee4b44acccec6f2d58bdcf60157191a7c6
Theodore <bethejustice@naver.com>
2019-02-11 00:57:08 +0900

    RecallView 구현

---
+++ app/src/main/java/teamh/boostcamp/myapplication/view/recall/RecallFragment.java
@@ -0,0 +1,61 @@
+package teamh.boostcamp.myapplication.view.recall;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import teamh.boostcamp.myapplication.R;
+import teamh.boostcamp.myapplication.data.local.room.AppDatabase;
+import teamh.boostcamp.myapplication.data.model.Recall;
+import teamh.boostcamp.myapplication.data.repository.RecallRepositoryImpl;
+import teamh.boostcamp.myapplication.databinding.FragmentRecallBinding;
+
+public class RecallFragment extends Fragment implements RecallView {
+
+    private RecallPresenter recallPresenter;
+    private FragmentRecallBinding binding;
+
+    public RecallFragment() {
+    }
+
+    public static RecallFragment newInstance() {
+        return new RecallFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_recall, container, false);
+        initViews();
+        recallPresenter.loadRecallList();
+        return binding.getRoot();
+    }
+
+    @Override
+    public void addRecallList(@NonNull List<Recall> recallList) {
+
+    }
+
+    private void initViews() {
+        initPresenter();
+    }
+
+    private void initRecyclerView() {
+        binding.rvCard.setLayoutManager(new LinearLayoutManager(getContext()));
+    }
+
+    private void initPresenter() {
+        recallPresenter = new RecallPresenter(this,
+                RecallRepositoryImpl.getInstance(AppDatabase.getInstance(getContext())));
+    }
+}
