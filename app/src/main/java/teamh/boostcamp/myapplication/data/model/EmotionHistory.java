7db369d3b8dc845eb48aa022b531fd4888cc1ddb
Woo <jhsw0375@gmail.com>
2019-02-10 11:12:45 +0900

    issue-120-EmotionHistory 데이터 클래스에 선택한 감정과 분석된 감정 구분

--- app/src/main/java/teamh/boostcamp/myapplication/data/model/EmotionHistory.java
+++ app/src/main/java/teamh/boostcamp/myapplication/data/model/EmotionHistory.java
@@ -9,12 +9,17 @@ public class EmotionHistory {
     @NonNull
     private Date date; // 날짜
     @NonNull
-    private Emotion emotion; // 감정
+    private Emotion selectedEmotion; // 사용자가 선택한 감정
+    @NonNull
+    private Emotion analyzedEmotion; // AI에 의해 분석된 감정
 
     // 생성자를 통한 주입
-    public EmotionHistory(@NonNull Date date, @NonNull Emotion emotion) {
+    public EmotionHistory(@NonNull Date date,
+                          @NonNull Emotion selectedEmotion,
+                          @NonNull Emotion analyzedEmotion) {
         this.date = date;
-        this.emotion = emotion;
+        this.selectedEmotion = selectedEmotion;
+        this.analyzedEmotion = analyzedEmotion;
     }
 
     @NonNull
@@ -23,8 +28,13 @@ public class EmotionHistory {
     }
 
     @NonNull
-    public Emotion getEmotion() {
-        return emotion;
+    public Emotion getSelectedEmotion() {
+        return selectedEmotion;
+    }
+
+    @NonNull
+    public Emotion getAnalyzedEmotion() {
+        return analyzedEmotion;
     }
 
 }
