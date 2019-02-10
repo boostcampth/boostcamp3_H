f63d6f2970017f36f09f83f89a3abda36363a97d
Woo <jhsw0375@gmail.com>
2019-02-10 13:26:05 +0900

    issue-122-StatisticsRepositoryImpl 클래스 작성

---
+++ app/src/main/java/teamh/boostcamp/myapplication/data/repository/StatisticsRepositoryImpl.java
@@ -0,0 +1,44 @@
+package teamh.boostcamp.myapplication.data.repository;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import io.reactivex.Single;
+import io.reactivex.schedulers.Schedulers;
+import teamh.boostcamp.myapplication.data.local.room.dao.StatisticsDao;
+import teamh.boostcamp.myapplication.data.model.CountedTag;
+import teamh.boostcamp.myapplication.data.model.EmotionHistory;
+
+public class StatisticsRepositoryImpl implements StatisticsRepository {
+
+    private static volatile StatisticsRepositoryImpl INSTANCE;
+    @NonNull
+    private StatisticsDao statisticsDao;
+
+    private StatisticsRepositoryImpl(@NonNull StatisticsDao statisticsDao) {
+        this.statisticsDao = statisticsDao;
+    }
+
+    public static StatisticsRepositoryImpl getInstance(@NonNull StatisticsDao statisticsDao) {
+        if (INSTANCE == null) {
+            synchronized (StatisticsRepositoryImpl.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new StatisticsRepositoryImpl(statisticsDao);
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    @NonNull
+    @Override
+    public Single<List<EmotionHistory>> loadRecentEmotionHistoryList() {
+        return statisticsDao.loadRecentEmotionHistoryList().subscribeOn(Schedulers.io());
+    }
+
+    @NonNull
+    @Override
+    public Single<List<CountedTag>> loadRecentCountedTagList() {
+        return statisticsDao.loadRecentCountedTagList().subscribeOn(Schedulers.io());
+    }
+}
