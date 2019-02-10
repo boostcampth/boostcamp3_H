14cd9184fb92a069f16415db7c88d21d3cb64dfc
Woo <jhsw0375@gmail.com>
2019-02-10 18:44:30 +0900

    issue-122-사용하지 않는 import 제거

--- app/src/main/java/teamh/boostcamp/myapplication/data/repository/StatisticsRepository.java
+++ app/src/main/java/teamh/boostcamp/myapplication/data/repository/StatisticsRepository.java
@@ -5,15 +5,13 @@ import java.util.List;
 import androidx.annotation.NonNull;
 import io.reactivex.Single;
 import teamh.boostcamp.myapplication.data.model.CountedTag;
-import teamh.boostcamp.myapplication.data.model.EmotionHistory;
 
 /**
- *
  * @author 이승우
  * @version 1.0.1
  * 최신 2주 간의 감정 정보를 불러온다.
  * 최신 2주 간의 해시태그 정보를 불러온다.
- * */
+ */
 public interface StatisticsRepository {
 
     @NonNull
