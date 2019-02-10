e45bc9dbf998b1f4376b77963e891d53bb3e1710
Woo <jhsw0375@gmail.com>
2019-02-11 00:06:56 +0900

    issue-122-Repository 구현

--- app/src/main/java/teamh/boostcamp/myapplication/data/repository/StatisticsRepositoryImpl.java
+++ app/src/main/java/teamh/boostcamp/myapplication/data/repository/StatisticsRepositoryImpl.java
@@ -2,6 +2,8 @@ package teamh.boostcamp.myapplication.data.repository;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 import androidx.annotation.NonNull;
@@ -19,12 +21,10 @@ public class StatisticsRepositoryImpl implements StatisticsRepository {
     private static volatile StatisticsRepositoryImpl INSTANCE;
     @NonNull
     final AppDatabase appDatabase;
-    private List<DiaryEntity> diaryEntityList;
-    private List<EmotionHistory> totalEmotionHistoryList;
+
 
     private StatisticsRepositoryImpl(@NonNull AppDatabase appDatabase) {
         this.appDatabase = appDatabase;
-        totalEmotionHistoryList = new ArrayList<>();
     }
 
     public static StatisticsRepositoryImpl getInstance(@NonNull AppDatabase appDatabase) {
@@ -42,13 +42,47 @@ public class StatisticsRepositoryImpl implements StatisticsRepository {
     @Override
     public Single<List<CountedTag>> loadRecentCountedTagList(
             @NonNull Date lastItemSavedTime) {
-        return null;
+
+        List<DiaryEntity> diaryEntityList = new ArrayList<>();
+
+
+        diaryEntityList = (List<DiaryEntity>) appDatabase.diaryDao().loadDiaryList(lastItemSavedTime, RECENT_TERM);
+
+        int size = diaryEntityList.size();
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+
+        for (int i = 0; i < size; i++) {
+            List<String> tags = diaryEntityList.get(i).getTags();
+
+            for (int j = 0; j < tags.size(); j++) {
+                if (map.containsKey(tags.get(j))) {
+                    int count = map.get(tags.get(j)).intValue();
+                    map.put(tags.get(j), count + 1);
+                } else {
+                    map.put(tags.get(j), 1);
+                }
+            }
+        }
+        final List<CountedTag> countedTags = new ArrayList<>();
+
+        Iterator<String> iterator = map.keySet().iterator();
+        while (iterator.hasNext()){
+            String tagName = iterator.next();
+            int count = map.get(tagName);
+            countedTags.add(new CountedTag(tagName,count));
+        }
+        return (Single<List<CountedTag>>) countedTags;
     }
 
     @NonNull
     @Override
     public Single<List<EmotionHistory>> loadRecentEmotionHistoryList(
             @NonNull Date lastItemSavedTime) {
+
+        List<DiaryEntity> diaryEntityList = new ArrayList<>();
+        final List<EmotionHistory> totalEmotionHistoryList = new ArrayList<>();
 
         diaryEntityList = (List<DiaryEntity>) appDatabase.diaryDao()
                 .loadDiaryList(lastItemSavedTime, RECENT_TERM);
