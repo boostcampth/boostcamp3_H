dbd378ac2bfd11f9de7af7542ea367f4dbe0c791
Woo <jhsw0375@gmail.com>
2019-02-11 02:10:14 +0900

    issue-130-Presenter 및 View 인터페이스 수정

--- app/src/main/java/teamh/boostcamp/myapplication/view/graph/StatisticsPresenter.java
+++ app/src/main/java/teamh/boostcamp/myapplication/view/graph/StatisticsPresenter.java
@@ -14,8 +14,17 @@ public class StatisticsPresenter {
     @NonNull
     private StatisticsView statisticsView;
 
-    // 생성자.
-    public StatisticsPresenter(){
+    // 생성자를 통한 주입.
+    public StatisticsPresenter(@NonNull StatisticsRepository statisticsRepository,
+                               @NonNull StatisticsView statisticsView){
+        this.statisticsRepository = statisticsRepository;
+        this.statisticsView = statisticsView;
+    }
 
+    void loadStatisticsData(){
+        /**
+         * Repository에게 데이터 요청해서 받아오는 로직.
+         * 그리고 view에 갱신
+         * */
     }
 }
