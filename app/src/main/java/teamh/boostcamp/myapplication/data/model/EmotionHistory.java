8e9cb4f3ecf3187b2d30fd93563299ed5246b798
Woo <jhsw0375@gmail.com>
2019-02-10 16:37:18 +0900

    issue-118-EmotionHistory 데이터 클래스 수정

--- app/src/main/java/teamh/boostcamp/myapplication/data/model/EmotionHistory.java
+++ app/src/main/java/teamh/boostcamp/myapplication/data/model/EmotionHistory.java
@@ -4,22 +4,27 @@ import java.util.Date;
 
 import androidx.annotation.NonNull;
 
+/**
+ * @author 이승우
+ *
+ * emotionType(1) - 선택한 감정
+ * emotionType(2) - 분석된 감정
+ * */
 public class EmotionHistory {
 
     @NonNull
     private Date date; // 날짜
     @NonNull
-    private Emotion selectedEmotion; // 사용자가 선택한 감정
-    @NonNull
-    private Emotion analyzedEmotion; // AI에 의해 분석된 감정
+    private Emotion emotion; // 감정
+    private int emotionType; // 감정 타입
 
     // 생성자를 통한 주입
     public EmotionHistory(@NonNull Date date,
-                          @NonNull Emotion selectedEmotion,
-                          @NonNull Emotion analyzedEmotion) {
+                          @NonNull Emotion emotion,
+                          int emotionType) {
         this.date = date;
-        this.selectedEmotion = selectedEmotion;
-        this.analyzedEmotion = analyzedEmotion;
+        this.emotion = emotion;
+        this.emotionType = emotionType;
     }
 
     @NonNull
@@ -28,13 +33,12 @@ public class EmotionHistory {
     }
 
     @NonNull
-    public Emotion getSelectedEmotion() {
-        return selectedEmotion;
+    public Emotion getEmotion() {
+        return emotion;
     }
 
-    @NonNull
-    public Emotion getAnalyzedEmotion() {
-        return analyzedEmotion;
+    public int getEmotionType(){
+        return emotionType;
     }
 
 }
