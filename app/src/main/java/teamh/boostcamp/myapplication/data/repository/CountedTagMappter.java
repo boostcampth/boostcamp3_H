23b679e7602840050482d2e73f17c5e2795483ad
Woo <jhsw0375@gmail.com>
2019-02-10 17:15:23 +0900

    issue-122-CountedTagMapper 생성 - 반환되는 tag를 CountedTagMapeer를 통해 Mapping 하도록 수정했습니다. - StatisticsRepositoryImpl 에서 오버라이드 함수에서 map을 사용했습니다. - StatisticsDao의 함수 반환값 수정

---
+++ app/src/main/java/teamh/boostcamp/myapplication/data/repository/CountedTagMappter.java
@@ -0,0 +1,23 @@
+package teamh.boostcamp.myapplication.data.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import teamh.boostcamp.myapplication.data.model.CountedTag;
+
+public class CountedTagMappter {
+
+    public static List<CountedTag> tagToCountedTagMapper(@NonNull List<String> tagList) {
+        int size = tagList.size();
+
+        ArrayList<CountedTag> countedTagList = new ArrayList<>(size);
+
+        for (int i = 0; i < size; i++) {
+            String tag = tagList.get(i);
+
+            countedTagList.add(new CountedTag(tag, i));
+        }
+        return countedTagList;
+    }
+}
