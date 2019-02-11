555435a66c9c956b53fd0cf94d2e07abe1fc9775
카퀴카퀴 <kokoxg2@naver.com>
2019-02-11 11:23:53 +0900

    issue-132/create-impl-insert

--- app/src/main/java/teamh/boostcamp/myapplication/data/repository/DiaryRepository.java
+++ app/src/main/java/teamh/boostcamp/myapplication/data/repository/DiaryRepository.java
@@ -4,7 +4,9 @@ import java.util.Date;
 import java.util.List;
 
 import androidx.annotation.NonNull;
+import io.reactivex.Completable;
 import io.reactivex.Single;
+import teamh.boostcamp.myapplication.data.local.room.entity.DiaryEntity;
 import teamh.boostcamp.myapplication.data.model.Diary;
 
 public interface DiaryRepository {
@@ -12,4 +14,7 @@ public interface DiaryRepository {
     @NonNull
     Single<List<Diary>> loadDiaryList(@NonNull final Date startAfter,
                                       final int pageSize);
+
+    @NonNull
+    Completable insertDiary(@NonNull final DiaryEntity diaryEntity);
 }
