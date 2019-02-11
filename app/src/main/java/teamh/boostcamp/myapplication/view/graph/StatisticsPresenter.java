6ded3f55479a497cc7e4cffdb061d6e8cdd23339
Woo <jhsw0375@gmail.com>
2019-02-11 11:14:49 +0900

    issue-130-StatisticsPresenter 구현 및 StatisticsRepositoryImpl 코드 수정

--- app/src/main/java/teamh/boostcamp/myapplication/view/graph/StatisticsPresenter.java
+++ app/src/main/java/teamh/boostcamp/myapplication/view/graph/StatisticsPresenter.java
@@ -1,6 +1,10 @@
 package teamh.boostcamp.myapplication.view.graph;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import androidx.annotation.NonNull;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import teamh.boostcamp.myapplication.data.repository.StatisticsRepository;
 
 public class StatisticsPresenter {
@@ -26,5 +30,23 @@ public class StatisticsPresenter {
          * Repository에게 데이터 요청해서 받아오는 로직.
          * 그리고 view에 갱신
          * */
+
+        statisticsRepository.loadRecentEmotionHistoryList(new Date())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(emotionHistoryList -> {
+                    // TODO 부에 처리
+                }, throwable -> {
+                    // TODO 에러 처리
+                });
+    }
+
+    void loadTagList(){
+        statisticsRepository.loadRecentCountedTagList(new Date())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(countedTags -> {
+                    // TODO : 뷰에 처리
+                }, throwable -> {
+                    // TODO : 에러 처리
+                });
     }
 }
