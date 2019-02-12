a7163fb8830645f8407263be44eb0159222f5406
Theodore <bethejustice@naver.com>
2019-02-13 01:14:02 +0900

    bottom navigation setting

--- app/src/main/java/teamh/boostcamp/myapplication/view/main/MainActivity.java
+++ app/src/main/java/teamh/boostcamp/myapplication/view/main/MainActivity.java
@@ -2,12 +2,16 @@ package teamh.boostcamp.myapplication.view.main;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
-import androidx.viewpager.widget.ViewPager;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import teamh.boostcamp.myapplication.R;
 import teamh.boostcamp.myapplication.databinding.ActivityMainBinding;
 import teamh.boostcamp.myapplication.view.diarylist.DiaryListFragment;
@@ -20,65 +24,61 @@ public class MainActivity extends AppCompatActivity implements MainActivityView{
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    private MainTabAdapter tabAdapter;
     private MainPresenter presenter;
-
     private ActivityMainBinding binding;
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    private RecallFragment recallFragment;
+    private DiaryListFragment diaryListFragment;
+    private StatisticsFragment statisticsFragment;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            switch (item.getItemId()) {
+                case R.id.navigation_recall:
+                    transaction.replace(R.id.frame_layout, recallFragment).commitAllowingStateLoss();
+                    binding.tvMainTitle.setText(getString(R.string.Memories));
+                    return true;
+                case R.id.navigation_diary:
+                    transaction.replace(R.id.frame_layout, diaryListFragment).commitAllowingStateLoss();
+                    binding.tvMainTitle.setText(getString(R.string.main_toolbar_diary_title));
+                    return true;
+                case R.id.navigation_statistics:
+                    transaction.replace(R.id.frame_layout, statisticsFragment).commitAllowingStateLoss();
+                    binding.tvMainTitle.setText(getString(R.string.main_toolbar_graph_title));
+                    return true;
+            }
+            return false;
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         presenter = new MainPresenter(this);
-
-
         // bindingUtil 설정
         binding = DataBindingUtil.setContentView(this, getLayoutId());
+        recallFragment = RecallFragment.newInstance();
+        diaryListFragment = DiaryListFragment.newInstance();
+        statisticsFragment = StatisticsFragment.newInstance();
 
-        initView();
+        initBottomNavigation();
     }
 
     protected int getLayoutId() {
         return R.layout.activity_main;
     }
 
-    private void initView() {
-        Log.e(TAG, "initView");
-        tabAdapter = new MainTabAdapter(getSupportFragmentManager());
-        tabAdapter.addFragment(RecallFragment.newInstance());
-        tabAdapter.addFragment(DiaryListFragment.newInstance());
-        tabAdapter.addFragment(StatisticsFragment.newInstance());
-        binding.vpMain.setAdapter(tabAdapter);
-        binding.vpMain.setOffscreenPageLimit(3);
-        // 녹음 화면을 첫 화면으로 설정
-        binding.vpMain.setCurrentItem(1);
-        binding.setActivity(MainActivity.this);
-        binding.vpMain.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                switch (position) {
-                    case 0:
-                        binding.tvMainTitle.setText(getResources().getString(R.string.Memories));
-                        break;
-                    case 1:
-                        binding.tvMainTitle.setText(getResources().getString(R.string.main_toolbar_diary_title));
-                        break;
-                    case 2:
-                        binding.tvMainTitle.setText(getResources().getString(R.string.main_toolbar_graph_title));
-                        break;
-                }
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
+    private void initBottomNavigation(){
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+        binding.bottomNavigationView.setSelectedItemId(R.id.navigation_diary);
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout, diaryListFragment).commitAllowingStateLoss();
     }
 
     // 상단 Toolbar 클릭 시 설정 화면으로 이동
