421cdb802a4b4b4c5a8f38200bfb21d64d1a2481
Woo <jhsw0375@gmail.com>
2019-02-12 21:20:27 +0900

    issue-162- 비밀번호 설정에 필요한 파일 생성 - LockHelper - LockHelperImpl - LockManager

---
+++ app/src/main/java/teamh/boostcamp/myapplication/view/password/LockHelper.java
@@ -0,0 +1,38 @@
+package teamh.boostcamp.myapplication.view.password;
+
+import java.util.HashSet;
+
+/*
+ * 추상 클래스
+ * */
+public abstract class LockHelper {
+
+    public static final int ENABLE_PASSLOCK = 0; // 비밀번호 설정 가능
+    public static final int DISABLE_PASSLOCK = 1; // 비밀번호 해제 가능
+    public static final int CHANGE_PASSWORD = 2; // 비밀번호 변경 가능
+    public static final int UNLOCK_PASSWORD = 3; // 비밀번호 풀기
+
+    public static final String MESSAGE = "message";
+    public static final String TYPE = "type";
+
+    public static final int DEFAULT_TIMEOUT = 0; // 2000ms
+
+    protected int lockTimeOut;
+    protected HashSet<String> ignoredActivities;
+
+    public LockHelper() {
+        // 무시되는 액티비티들을 모아놓음.
+        ignoredActivities = new HashSet<String>();
+        lockTimeOut = DEFAULT_TIMEOUT;
+    }
+
+    public abstract void enable();
+
+    public abstract void disable();
+
+    public abstract boolean setPassword(String password);
+
+    public abstract boolean checkPassword(String password);
+
+    public abstract boolean isPasswordSet();
+}
