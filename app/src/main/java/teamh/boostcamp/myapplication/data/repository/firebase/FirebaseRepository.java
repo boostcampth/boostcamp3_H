6266009b08a081668dc157a45aca154a854b39d7
카퀴카퀴 <kokoxg2@naver.com>
2019-02-14 17:21:45 +0900

    issue-184/firebase-repository-interface 작성`

---
+++ app/src/main/java/teamh/boostcamp/myapplication/data/repository/firebase/FirebaseRepository.java
@@ -0,0 +1,21 @@
+package teamh.boostcamp.myapplication.data.repository.firebase;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import io.reactivex.Completable;
+import io.reactivex.Maybe;
+import teamh.boostcamp.myapplication.data.local.room.entity.DiaryEntity;
+import teamh.boostcamp.myapplication.data.model.Diary;
+
+public interface FirebaseRepository {
+
+    @NonNull
+    Maybe<List<String>> loadAllDiaryId();
+
+    @NonNull
+    Maybe<List<Diary>> loadAllDiaryList();
+
+    @NonNull
+    Completable insertDiaries(@NonNull List<DiaryEntity> diaryEntities);
+}
