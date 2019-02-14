cd5155ee5a0be3cf7379b86195f9dde21d3a6f20
Woo <jhsw0375@gmail.com>
2019-02-14 18:24:29 +0900

    issue-183-Firebase Login 기능 구현

--- app/src/main/java/teamh/boostcamp/myapplication/view/setting/SettingActivity.java
+++ app/src/main/java/teamh/boostcamp/myapplication/view/setting/SettingActivity.java
@@ -6,6 +6,21 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import teamh.boostcamp.myapplication.R;
@@ -17,13 +32,16 @@ import teamh.boostcamp.myapplication.view.password.LockManager;
 import teamh.boostcamp.myapplication.view.password.PasswordActivity;
 import teamh.boostcamp.myapplication.view.password.PasswordSelectActivity;
 
-public class SettingActivity extends AppCompatActivity  implements SettingView{
+public class SettingActivity extends AppCompatActivity implements SettingView {
 
     private static final String TAG = SettingActivity.class.getClass().getSimpleName();
-    ActivitySettingBinding binding;
+    private static final int SIGN_IN_CODE = 4899;
+    private ActivitySettingBinding binding;
     private AppInitializer appInitializer;
     private LockManager lockManager;
-    private SettingPresenter settingPresenter;
+
+    private GoogleSignInClient mGoogleSignInClient;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,23 +50,52 @@ public class SettingActivity extends AppCompatActivity  implements SettingView{
 
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (appInitializer.getAppInitializer(getApplicationContext()).getApplicationStatus()
+                == AppInitializer.ApplicationStatus.RETURNED_TO_FOREGROUND) {
+            if (lockManager.getLockHelper().isPasswordSet()) {
+                // 저장된 비밀번호가 존재하는지 확인.
+                Intent intent = new Intent(getApplicationContext(), PasswordActivity.class);
+                intent.putExtra(LockHelper.EXTRA_TYPE, LockHelper.UNLOCK_PASSWORD);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            } else {
+                Log.v(TAG, getApplicationContext().getResources().getString(R.string.password_not_set_text));
+            }
+        }
+    }
+
     private void init() {
-        settingPresenter = new SettingPresenter();
+        initFirebaseAuth();
         initBinding();
         initLock();
     }
 
-    private void initBinding(){
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_setting);
+    private void initFirebaseAuth() {
+        auth = FirebaseAuth.getInstance();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    }
+
+    private void initBinding() {
+        binding = DataBindingUtil.setContentView(SettingActivity.this, R.layout.activity_setting);
         binding.setActivity(SettingActivity.this);
     }
 
-    private void initLock(){
+    private void initLock() {
         appInitializer = new AppInitializer();
         lockManager = LockManager.getInstance();
         lockManager.enableLock(getApplication());
     }
 
+    // onClick
     public void onButtonClick(View view) {
         switch (view.getId()) {
             case R.id.iv_close_button:
@@ -62,31 +109,64 @@ public class SettingActivity extends AppCompatActivity  implements SettingView{
                 startActivity(new Intent(SettingActivity.this, PasswordSelectActivity.class));
                 break;
             case R.id.rl_setting_login:
-                showToast("Login 준비 중");
+                signIn();
+                showToastMessage(R.string.login_success);
                 break;
             case R.id.rl_setting_logout:
-                showToast("Logout 준비 중");
+                auth.signOut();
+                showToastMessage(R.string.logout_success);
                 break;
         }
     }
 
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, SIGN_IN_CODE);
+    }
+
     @Override
-    protected void onResume() {
-        super.onResume();
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-        if (appInitializer.getAppInitializer(getApplicationContext()).getApplicationStatus()
-                == AppInitializer.ApplicationStatus.RETURNED_TO_FOREGROUND) {
-            if (lockManager.getLockHelper().isPasswordSet()) {
-                // 저장된 비밀번호가 존재하는지 확인.
-                Intent intent = new Intent(getApplicationContext(), PasswordActivity.class);
-                intent.putExtra(LockHelper.EXTRA_TYPE, LockHelper.UNLOCK_PASSWORD);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-            } else {
-                Log.v(TAG, getApplicationContext().getResources().getString(R.string.password_not_set_text));
-            }
+        if (requestCode == SIGN_IN_CODE) {
+
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
         }
+    }
 
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            firebaseAuthWithGoogle(account);
+        } catch (ApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = auth.getCurrentUser();
+                        updateUI(user);
+                    } else {
+
+                        updateUI(null);
+                    }
+                });
+
+    }
+
+    private void updateUI(Object object) {
+        if (object != null) {
+            showToast("Success");
+        } else {
+            showToast("Fail");
+        }
     }
 
     @Override
@@ -99,4 +179,9 @@ public class SettingActivity extends AppCompatActivity  implements SettingView{
     private void showToast(String message) {
         Toast.makeText(SettingActivity.this, message, Toast.LENGTH_SHORT).show();
     }
+
+    private void showToastMessage(@StringRes final int stringId){
+        Toast.makeText(this,getString(stringId),Toast.LENGTH_SHORT).show();
+    }
+
 }
