0fa009367f1f5622e4520bcf09b24e11850834b6
Woo <jhsw0375@gmail.com>
2019-02-20 01:34:17 +0900

    issue-230 - 에러 수정 중

--- app/src/main/java/teamh/boostcamp/myapplication/view/statistics/StatisticsFragment.java
+++ app/src/main/java/teamh/boostcamp/myapplication/view/statistics/StatisticsFragment.java
@@ -69,7 +69,6 @@ public class StatisticsFragment extends Fragment implements StatisticsView {
     public void onDetach() {
         super.onDetach();
         statisticsPresenter.viewDestroyed();
-        simpleDateFormat = null;
         context = null;
     }
 
@@ -157,9 +156,9 @@ public class StatisticsFragment extends Fragment implements StatisticsView {
 
         // x축 설정
         xAxis.setAxisMinimum(0f);
-        xAxis.setAxisMaximum(dates.length - 1);
+        xAxis.setAxisMaximum(dates.length-1);
         xAxis.setCenterAxisLabels(true);
-        xAxis.setLabelCount(dates.length - 1, true);
+        xAxis.setLabelCount(dates.length-1, true);
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
         xAxis.setValueFormatter(new GraphAxisValueFormatter(dates));
         xAxis.setDrawGridLines(false);
@@ -169,7 +168,7 @@ public class StatisticsFragment extends Fragment implements StatisticsView {
         yLeftAxis.setAxisMinimum(0f);
         yLeftAxis.setValueFormatter(new GraphAxisValueFormatter(emojis));
         yLeftAxis.setTextSize(20f);
-        yLeftAxis.setAxisMaximum(4.0f); // max 값
+        yLeftAxis.setAxisMaximum(5.0f); // max 값
         yLeftAxis.setAxisMinimum(0.0f); // min 값
         yLeftAxis.setGranularityEnabled(true);
         yLeftAxis.setDrawGridLines(true);
@@ -187,9 +186,13 @@ public class StatisticsFragment extends Fragment implements StatisticsView {
         BarData barData = new BarData(selectedDataSetBar, analyzedDataSetBar);
         barData.setBarWidth(barWidth); // set the width of each bar
         barData.setDrawValues(false);
-        binding.lcEmotionGraph.setVisibleXRangeMaximum(5);
+        binding.lcEmotionGraph.setVisibleXRangeMaximum(5f);
+        //binding.lcEmotionGraph.setScaleEnabled(false);
         binding.lcEmotionGraph.setHorizontalScrollBarEnabled(false);
         binding.lcEmotionGraph.setPinchZoom(false);
+        binding.lcEmotionGraph.setDragEnabled(false);
+        //binding.lcEmotionGraph.setDragXEnabled(false);
+        //binding.lcEmotionGraph.setDragYEnabled(false);
         binding.lcEmotionGraph.moveViewToX(0);
         binding.lcEmotionGraph.setDescription(null);
         binding.lcEmotionGraph.setData(barData);
@@ -211,11 +214,22 @@ public class StatisticsFragment extends Fragment implements StatisticsView {
 
             //tagTextView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, hashTagItems.get(i).getCount() * 10);
             final int tagCount = hashTagItems.get(i).getCount();
+            Log.v(TAG, String.valueOf(tagCount));
 
-            if (tagCount == 1) {
-                tagTextView.setTextColor(context.getResources().getColor(R.color.hashtag));
-            } else if (tagCount >= 3) {
-                tagTextView.setTextColor(context.getResources().getColor(R.color.hashtag_dark));
+
+            switch (tagCount) {
+                case 1:
+                    tagTextView.setTextColor(context.getResources().getColor(R.color.hashtag_light));
+                    break;
+                case 2:
+                    tagTextView.setTextColor(context.getResources().getColor(R.color.hashtag));
+                    break;
+                case 3:
+                    tagTextView.setTextColor(context.getResources().getColor(R.color.hashtag_dark));
+                    break;
+                default:
+                    tagTextView.setTextColor(context.getResources().getColor(R.color.hashtag_dark));
+                    break;
             }
             tagTextView.setText(hashTagItems.get(i).getTagName());
             binding.hashTagCustomLayout.addView(tagView);
