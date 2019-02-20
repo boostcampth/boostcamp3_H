219e683f511e031886b20f5fcfb1e2bf75a9c988
Woo <jhsw0375@gmail.com>
2019-02-20 13:16:03 +0900

    issue-230 - 그래프 문제 해결

--- app/src/main/java/teamh/boostcamp/myapplication/view/statistics/StatisticsFragment.java
+++ app/src/main/java/teamh/boostcamp/myapplication/view/statistics/StatisticsFragment.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.Pair;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +15,7 @@ import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -115,7 +115,6 @@ public class StatisticsFragment extends Fragment implements StatisticsView {
     @Override
     public void updateStatisticsData(@NonNull List<Pair<EmotionHistory, EmotionHistory>> emotionHistoryList) {
         final int size = emotionHistoryList.size();
-
         //  Bar
         List<BarEntry> selectedEmotionListBar = new ArrayList<>();
         List<BarEntry> analyzedEmotionListBar = new ArrayList<>();
@@ -135,19 +134,23 @@ public class StatisticsFragment extends Fragment implements StatisticsView {
             // Bar
             selectedEmotionListBar.add(new BarEntry(i, selectedEmotion));
             analyzedEmotionListBar.add(new BarEntry(i, analyzedEmotion));
+            Log.v("Test Tag", dates[i]);
         }
 
         BarDataSet selectedDataSetBar = new BarDataSet(selectedEmotionListBar, null);
         selectedDataSetBar.setColor(getResources().getColor(R.color.main_dark));
+        selectedDataSetBar.setHighlightEnabled(false);
 
         BarDataSet analyzedDataSetBar = new BarDataSet(analyzedEmotionListBar, null);
         analyzedDataSetBar.setColor(getResources().getColor(R.color.main));
+        analyzedDataSetBar.setHighlightEnabled(false);
 
         final XAxis xAxis;
         final YAxis yLeftAxis, yRightAxis;
-        float groupSpace = 0.26f;
+        //float groupSpace = 0.2f;
+        float groupSpace = 0.06f;
         float barSpace = 0.02f; // x2 dataset
-        float barWidth = 0.35f; // x2 dataset
+        float barWidth = 0.45f; // x2 dataset
         final Legend legend = binding.lcEmotionGraph.getLegend();
 
         xAxis = binding.lcEmotionGraph.getXAxis();
@@ -156,19 +159,20 @@ public class StatisticsFragment extends Fragment implements StatisticsView {
 
         // x축 설정
         xAxis.setAxisMinimum(0f);
-        xAxis.setAxisMaximum(dates.length-1);
+        xAxis.setAxisMaximum(dates.length);
         xAxis.setCenterAxisLabels(true);
-        xAxis.setLabelCount(dates.length-1, true);
+        xAxis.setLabelCount(dates.length);
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-        xAxis.setValueFormatter(new GraphAxisValueFormatter(dates));
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(dates));
         xAxis.setDrawGridLines(false);
         xAxis.setYOffset(5f);
+        xAxis.setGranularity(1f); // 해결책.. 왜인지는 아직 파악하지 못함.
 
         // y축 왼쪽 설정
         yLeftAxis.setAxisMinimum(0f);
-        yLeftAxis.setValueFormatter(new GraphAxisValueFormatter(emojis));
+        yLeftAxis.setValueFormatter(new IndexAxisValueFormatter(emojis));
         yLeftAxis.setTextSize(20f);
-        yLeftAxis.setAxisMaximum(5.0f); // max 값
+        yLeftAxis.setAxisMaximum(4.0f); // max 값
         yLeftAxis.setAxisMinimum(0.0f); // min 값
         yLeftAxis.setGranularityEnabled(true);
         yLeftAxis.setDrawGridLines(true);
@@ -186,19 +190,18 @@ public class StatisticsFragment extends Fragment implements StatisticsView {
         BarData barData = new BarData(selectedDataSetBar, analyzedDataSetBar);
         barData.setBarWidth(barWidth); // set the width of each bar
         barData.setDrawValues(false);
-        binding.lcEmotionGraph.setVisibleXRangeMaximum(5f);
-        //binding.lcEmotionGraph.setScaleEnabled(false);
+        binding.lcEmotionGraph.setVisibleXRangeMaximum(5);
+        binding.lcEmotionGraph.setScaleEnabled(false);
         binding.lcEmotionGraph.setHorizontalScrollBarEnabled(false);
         binding.lcEmotionGraph.setPinchZoom(false);
-        binding.lcEmotionGraph.setDragEnabled(false);
-        //binding.lcEmotionGraph.setDragXEnabled(false);
-        //binding.lcEmotionGraph.setDragYEnabled(false);
         binding.lcEmotionGraph.moveViewToX(0);
         binding.lcEmotionGraph.setDescription(null);
         binding.lcEmotionGraph.setData(barData);
         binding.lcEmotionGraph.groupBars(0, groupSpace, barSpace);
         binding.lcEmotionGraph.animateY(1000);
         binding.lcEmotionGraph.invalidate(); // refresh
+
+
     }
 
     @Override
